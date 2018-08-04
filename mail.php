59347d6b450ee7600f8612eafd92f2cd7df1d416
Design PC <vigneshwaran.p@theprodux.com>
2018-08-04 17:40:59 +0530

    Estimate Form

--- mail.php
+++ mail.php
@@ -1,16 +1,16 @@
 <?php 
 $errors = '';
 $myemail = 'produxdesk@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
+if(empty($_POST['radio1'])  || 
+if(empty($_POST['radio2'])  || 
+   empty($_POST['email']))
 {
     $errors .= "\n Error: all fields are required";
 }
 
-$name = $_POST['name'];
+$q2 = $_POST['radio1'];
+$q1 = $_POST['radio2']; 
 $email_address = $_POST['email']; 
-$message = $_POST['message']; 
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
@@ -22,9 +22,9 @@ $email_address))
 if( empty($errors))
 {
 	$to = $myemail; 
-	$email_subject = "Contact form submission: $name";
+	$email_subject = "Contact form submission: $email_address";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name  \n Email: $email_address \n Message \n $message"; 
+	" Here are the details:\n Q1: $q1  \n Email: $q2 \nEmail: $email_address"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
