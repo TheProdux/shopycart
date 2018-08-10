c3c1680655688216635f9f0ce80972785cb167de
Design PC <vigneshwaran.p@theprodux.com>
2018-08-10 16:55:38 +0530

    Mail update

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,61 +1,56 @@
 <?php 
-if(isset($_POST['submit']) ){
-//$errors = '';
+/*$errors = '';
 $myemail = 'produxdesk@gmail.com';//<-----Put Your email address here.
-/*if(empty($_POST['name'])  || 
+if(empty($_POST['name'])  || 
 	empty($_POST['phone'])  || 
    empty($_POST['email']) || 
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
-}*/
+}
 
-$name = $_POST['username'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];  
 $email_address = $_POST['email']; 
-$q1 = $_POST['radio1'];
-$q2 = $_POST['radio2'];
-$q3 = $_POST['check1'];
-$q4 = $_POST['range'];
+$message = $_POST['message']; 
 
-/*if (!preg_match(
+if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
 $email_address))
 {
     $errors .= "\n Error: Invalid email address";
-}*/
-//echo 	"<h1> Here are the details:</h1> <br> <p>Name:<p> $name  \n Email: $email_address \n Q1: $q1 \n Q2: $q2 \n Q3: $q3 \n Q4: $q4"; 
+}
+
+if( empty($errors))
+{
 	$to = $myemail; 
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name  \n Email: $email_address \n Q1: $q1 \n Q2: $q2 \n Q3: $q3 \n Q4: $q4"; 
+	" Here are the details:\n Name: $name \n Mobile:$phone \n Email: $email_address \n Message \n $message"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
 	
 	mail($to,$email_subject,$email_body,$headers);
 	//redirect to the 'thank you' page
-	//header('Location: http://shopycart.com/new/index.html');
- 
-}
+	header('Location: index.html');
+} 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
 <head>
-	
+	<title>Contact form handler</title>
 </head>
 
 <body>
 <!-- This page is displayed only if there is some error -->
 <?php
-echo 	"<h1> Here are the details:</h1> <br> <p>Name:<p> $name  \n Email: $email_address \n Q1: $q1 \n Q2: $q2 \n Q3: $q3 \n Q4: $q4"; 
-
+echo nl2br($errors);
 ?>
 
 
 </body>
 </html>
-
-
 <?php 
  if(@$_POST['submit']){
 $errors = '';
@@ -93,5 +88,19 @@ if( empty($errors))
 	//redirect to the 'thank you' page
 	header('Location: index.html');
 } 
- }
+ }*/
+
+
+
+ $name = $_POST['username'];
+ $mail = $_POST['email'];
+ $radio1 = $_POST['radio1'];
+ $myemail = 'vickyrobotic7@gmail.com';
+ $to = $myemail; 
+ $email_subject = "Broucher Downloaded: $name";
+ $email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name  \n Email: $email_address \n radio1: $radio1 "; 
+	
+
+ mail($to,$email_subject,$email_body);
 ?>
